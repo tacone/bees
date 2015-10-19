ee59a0613c781787c4823098ea2c62ab803633d7
tacone <tacone@users.noreply.github.com>
2015-10-19 04:01:47 +0200

    dream on

--- daydream.php
+++ daydream.php
@@ -46,3 +46,19 @@ class Customers
 
 }
 
+class UsersController extends ResourceController {
+    function getResource() {
+        $endpoint = new Endpoint(new Customer());
+        $endpoint->string('name', 'first_name');
+        $endpoint->boolean('published');
+        $endpoint->dateTime('updated', 'Last update');
+        return $endpoint;
+    }
+    function getResourcesCollection() {
+        $collection = new Endpoint(new Customer());
+        $collection->string('name', 'first_name');
+        $collection->boolean('published', true);
+        $collection->dateTime('updated', 'Last update');
+        return $collection;
+    }
+}
