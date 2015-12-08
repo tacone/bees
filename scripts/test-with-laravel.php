0e68ba6191ad30f43dc93b7ea1b58c2b65ac6381
tacone <tacone@users.noreply.github.com>
2015-12-08 21:34:09 +0100

    fix travis with dev-master

--- scripts/test-with-laravel.php
+++ scripts/test-with-laravel.php
@@ -16,11 +16,11 @@
  * phpunit
  * ```
  */
-chdir(__DIR__ . '/..');
+chdir(__DIR__.'/..');
 
 $laravelVersion = !empty($argv[1]) ? $argv[1] : '';
 if (!$laravelVersion) {
-    echo "You need to supply a Laravel version as the first parameter, for instance: 5.1" . PHP_EOL;
+    echo 'You need to supply a Laravel version as the first parameter, for instance: 5.1'.PHP_EOL;
     echo PHP_EOL;
     exit(1);
 }
@@ -29,36 +29,35 @@ echo "Bootstrapping package to test with Laravel $laravelVersion";
 echo PHP_EOL;
 
 if (!file_exists('composer.phar')) {
-    echo 'downloading composer:' . PHP_EOL;
+    echo 'downloading composer:'.PHP_EOL;
     passthru('php -r "readfile(\'https://getcomposer.org/installer\');" | php');
 }
 echo PHP_EOL;
 
-echo "Backupping composer.json" . PHP_EOL;
-passthru("cp composer.json composer.json.backup");
+echo 'Backupping composer.json'.PHP_EOL;
+passthru('cp composer.json composer.json.backup');
 
 if (strpos($laravelVersion, '.')) {
     $tokens = explode('.', $laravelVersion);
     $tokens[0] -= 2;
-    $orchestralVersion = join('.', array_slice($tokens, 0, 2)).'.*';
+    $orchestralVersion = implode('.', array_slice($tokens, 0, 2)).'.*';
 } else {
-    $orchestralVersion = $laravelVersion;
+    //    $orchestralVersion = $laravelVersion;
+    $orchestralVersion = '3.2';
 }
 
 if (file_exists('vendor')) {
-    echo "Removing vendor folder..." . PHP_EOL;
-    passthru("rm vendor -rf");
+    echo 'Removing vendor folder...'.PHP_EOL;
+    passthru('rm vendor -rf');
 }
 
-echo "Installing Orchestral $orchestralVersion: " . PHP_EOL;
+echo "Installing Orchestral $orchestralVersion: ".PHP_EOL;
 passthru("php composer.phar require orchestra/testbench '$orchestralVersion'");
 
-echo "Installing Faker" . PHP_EOL;
+echo 'Installing Faker'.PHP_EOL;
 passthru("php composer.phar require fzaninotto/faker '@stable'");
 
-echo "Reverting composer.json" . PHP_EOL;
-passthru("cp composer.json composer.json.test");
-passthru("cp composer.json.backup composer.json");
-passthru("rm composer.json.backup");
-
-
+echo 'Reverting composer.json'.PHP_EOL;
+passthru('cp composer.json composer.json.test');
+passthru('cp composer.json.backup composer.json');
+passthru('rm composer.json.backup');
